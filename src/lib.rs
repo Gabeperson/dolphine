c36814ad71c164da4b486e374b40c30ebac4e389
Gabeperson <95374687+Gabeperson@users.noreply.github.com>
2023-02-07 14:45:16 -0800

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -23,7 +23,7 @@ use tokio_tungstenite;
 use tokio_tungstenite::tungstenite::Message;
 mod browsers;
 pub use browsers::Browser;
-pub use macros::function;
+pub use macros::function as function;
 
 type Function = fn(String) -> Result<String, RustCallError>;
 
